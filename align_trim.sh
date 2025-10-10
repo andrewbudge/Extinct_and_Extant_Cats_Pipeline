5b5a327168f5171bc844cc67c81d13fb3e8632cd
Andrew Budge <andrewcbudge@gmail.com>
2025-10-09 18:21:11 -0600

    Add files via upload
    shell script that will align and trim data

---
+++ align_trim.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env
+set -e
+
+INPUT_DIR="$1"      # e.g., raw fasta files
+ALIGNED_DIR="$2"    # e.g., where to save MAFFT alignments
+TRIMMED_DIR="$3"    # e.g., where to save ClipKIT-trimmed alignments
+
+mkdir -p "$ALIGNED_DIR" "$TRIMMED_DIR"
+
+# Create temp for clipkit
+python3 -m venv clipkitENV
+source clipkitENV/bin/activate
+pip install --quiet clipkit
+
+# Align with MAFFT
+for file in "$INPUT_DIR"/*.{fasta,fas,fa}; do
+    [[ -e "$file" ]] || continue
+    base=$(basename "$file")
+    base_no_ext="${base%.*}"
+    aligned_file="$ALIGNED_DIR/${base_no_ext}_aln.fasta"
+
+    mafft --thread 6 --auto "$file" > "$aligned_file"
+done
+
+# rm MAFFT logs
+find "$INPUT_DIR" -type f -name "*.log" -delete
+
+# Trim alignments with ClipKIT
+for aln in "$ALIGNED_DIR"/*_aln.fasta; do
+    [[ -e "$aln" ]] || continue
+    base=$(basename "$aln" _aln.fasta)
+    trimmed_file="$TRIMMED_DIR/${base}_trm.fas"
+
+    clipkit "$aln" -m smart-gap -o "$trimmed_file"
+done
+
+# Cleanup
+deactivate
+rm -rf clipkitENV/
+
